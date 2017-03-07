c22ccffdf5e72c83b7dfd13b5415a8125166d6c9
Miro Spönemann <miro.spoenemann@typefox.io>
2017-03-07 14:22:35 +0100

    Added client code for JSON-RPC communication

---
+++ client/src/jsonrpc/common/index.ts
@@ -0,0 +1,5 @@
+export * from './logger';
+export * from './stream';
+export * from './reader';
+export * from './writer';
+export * from './manager';
---
+++ client/src/jsonrpc/webSocket/writer.ts
@@ -0,0 +1,18 @@
+import {
+    AbstractStreamMessageWriter
+} from '../common';
+
+export class WebSocketMessageWriter extends AbstractStreamMessageWriter {
+
+    protected socket: WebSocket;
+
+    constructor(socket: WebSocket) {
+        super();
+        this.socket = socket;
+    }
+
+    protected send(content: string): void {
+        this.socket.send(content);
+    }
+
+}
