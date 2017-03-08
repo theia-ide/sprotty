ffced391cf7ea2ed4715dc0cc978b43e6f882174
Marc Dumais <marc.dumais@ericsson.com>
2017-03-08 11:01:20 -0500

    improved select command tests
    changed tests following rebase

--- client/test/commandTests.ts
+++ client/test/commandTests.ts
@@ -1,17 +1,17 @@
-import { GGraph, GNode } from "../src/graph/GModel"
-import { SelectCommand, SelectAction } from "../src/base/intent/Select";
+import { GGraph, GNode } from "../src/graph/model/ggraph"
+import { SelectCommand, SelectAction } from "../src/base/intent/select";
 import { expect, should } from 'chai';
 import 'mocha'
-import { GModelRoot } from "../src/base/model/GModel";
+import { GModelRoot } from "../src/base/model/gmodel";
 
-const myNode0 = { id: 'node0', type: 'circle', x: 100, y: 100, selected: true };
-const myNode1 = { id: 'node1', type: 'circle', x: 200, y: 200, selected: false };
+const myNode0 = { id: 'node0', type: 'node:circle', x: 100, y: 100, selected: true };
+const myNode1 = { id: 'node1', type: 'node:circle', x: 200, y: 200, selected: false };
 
 // setup the GModel
-const model = new GGraph({
+const initialModel = new GGraph({
     id: 'graph',
     type: 'graph',
-    shapes: [myNode0, myNode1]
+    children: [myNode1, myNode0]  // myNode0 is selected, so put at the end
 });
 
 // create the action
@@ -20,31 +20,27 @@ const mySelectAction = new SelectAction(
     ['node0']  // deselected list
 )
 
+const lastIndex = initialModel.children.length() - 1
+
 // create the select command
 const cmd = new SelectCommand(mySelectAction)
 
+// global so we can carry-over the model, as it's updated, 
+// from test case to test case (i,e, select, undo, redo, merge)
 var newModel: GModelRoot
 
 describe('test select command execution, undo and redo', () => {
     it('select command', () => {
         // execute command
-        newModel = cmd.execute(model)
+        newModel = cmd.execute(initialModel)
 
-        // check result
-        const nodes = <GNode[]>newModel.index.all().filter(
-            element => (element instanceof GNode)
-        )
-        const node0 = <GNode>newModel.index.getById('node0')
-        const node1 = <GNode>newModel.index.getById('node1')
+        // confirm selection is as expected
+        expect(true).to.equal(isNodeSelected('node1', newModel))
+        expect(false).to.equal(isNodeSelected('node0', newModel))
 
-        expect(node0.selected).to.equal(false)
-        expect(node1.selected).to.equal(true)
-
-        // note: not sure what this test achieves? make sure that 
-        // node selection doesn't mess with children ordering 
-        // in the model? 
-        expect(nodes.indexOf(node0)).to.equal(0)
-        expect(nodes.indexOf(node1)).to.equal(1)
+        // the selected node is moved at the end of the array
+        expect(lastIndex).to.equal(getNodeIndex('node1',newModel))
+        expect(0).to.equal(getNodeIndex('node0',newModel))
     });
 
     it('undo select command', () => {
@@ -55,28 +51,54 @@ describe('test select command execution, undo and redo', () => {
         const nodes = <GNode[]>newModel.index.all().filter(
             element => (element instanceof GNode)
         )
-        const node0 = <GNode>newModel.index.getById('node0')
-        const node1 = <GNode>newModel.index.getById('node1')
 
-        expect(node0.selected).to.equal(true)
-        expect(node1.selected).to.equal(false)
+        // confirm selection is as expected
+        expect(true).to.equal(isNodeSelected('node0', newModel))
+        expect(false).to.equal(isNodeSelected('node1', newModel))
+
+        // the selected node is moved at the end of the array
+        expect(lastIndex).to.equal(getNodeIndex('node0',newModel))
+        expect(0).to.equal(getNodeIndex('node1',newModel))
+
     });
+
     it('redo select command', () => {
         // test "redo"
         newModel = cmd.redo(newModel);
 
-        // check result
-        const nodes = <GNode[]>newModel.index.all().filter(
-            element => (element instanceof GNode)
-        )
-        const node0 = <GNode>newModel.index.getById('node0')
-        const node1 = <GNode>newModel.index.getById('node1')
+        // confirm selection is as expected
+        expect(true).to.equal(isNodeSelected('node1', newModel))
+        expect(false).to.equal(isNodeSelected('node0', newModel))
 
-        expect(node0.selected).to.equal(false)
-        expect(node1.selected).to.equal(true)
+        // the selected node is moved at the end of the array
+        expect(lastIndex).to.equal(getNodeIndex('node1',newModel))
+        expect(0).to.equal(getNodeIndex('node0',newModel))
     });
 
+    // "merge" is N/A for selection
+    it('merge select command (N/A)', () => {
+        // test "merge"
+        const result = cmd.merge(cmd);
+        expect(false).to.equal(result)
 
+        // confirm selection is as expected (i.e. unchanged)
+        expect(true).to.equal(isNodeSelected('node1', newModel))
+        expect(false).to.equal(isNodeSelected('node0', newModel))
+
+        // the selected node is moved at the end of the array  (i.e. unchanged)
+        expect(lastIndex).to.equal(getNodeIndex('node1',newModel))
+        expect(0).to.equal(getNodeIndex('node0',newModel))
+    });
 })
 
+function getNode(nodeId, model) {
+    return <GNode>model.index.getById(nodeId)
+}
 
+function isNodeSelected(nodeId, model) {
+    return getNode(nodeId, model).selected
+}
+
+function getNodeIndex(nodeId, model) { 
+    return model.children.indexOf(getNode(nodeId, model))
+}
