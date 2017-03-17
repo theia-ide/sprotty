31ee3a1824eecc30f8b677d1a6a064c7b54d7a0f
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-03-17 13:50:03 +0100

    [tests] easing test

---
+++ client/src/base/animations/easing.spec.ts
@@ -0,0 +1,15 @@
+import {easeInOut} from "./easing"
+import {expect} from "chai"
+
+describe('easing', () => {
+    it('test in/out',() => {
+        let lastValue = 0
+        for(let i=0; i< 10; ++i) {
+            const newValue = easeInOut(0.1 * i)
+            expect(newValue).to.be.at.least(0)
+            expect(newValue).to.be.at.most(1)
+            expect(newValue).to.be.at.least(lastValue)
+            lastValue = newValue
+        }
+    })
+})
