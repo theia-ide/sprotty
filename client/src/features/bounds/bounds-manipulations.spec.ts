493ec59bc34936bb3c1704ef2178c53007f29b83
Marc Dumais <marc.dumais@ericsson.com>
2017-03-30 07:44:55 -0400

    [tests] SetBoundsCommand: add coverage for BoundsInPageAware

--- client/src/features/bounds/bounds-manipulations.spec.ts
+++ client/src/features/bounds/bounds-manipulations.spec.ts
@@ -1,6 +1,6 @@
 import "mocha"
 import { expect } from "chai"
-import { SetBoundsCommand, SetBoundsAction } from "../index";
+import { SetBoundsCommand, SetBoundsAction,  BoundsInPageAware} from "../index";
 import { CommandExecutionContext } from "../../base/intent/commands"
 import { EMPTY_ROOT } from "../../base/model/smodel-factory"
 import { ConsoleLogger } from "../../utils/logging"
@@ -8,32 +8,38 @@ import { AnimationFrameSyncer } from "../../base/animations/animation-frame-sync
 import { SNode } from "../../graph/model/sgraph"
 import { SGraphFactory } from "../../graph/index";
 import { SNodeSchema } from "../../graph/model/sgraph"
+import { EMPTY_BOUNDS, Bounds } from "../../utils/index";
 
-const bounds0 = { x: 0, y: 0, width: 0, height: 0 }
+// ATM SNode is not "BoundsInPageAware", so let's make out own
+class mySNode extends SNode implements BoundsInPageAware {
+    boundsInPage: Bounds = EMPTY_BOUNDS
+}
+
+const boundsInitial = { x: 0, y: 0, width: 0, height: 0 }
 const bounds1 = { x: 10, y: 10, width: 10, height: 10 }
 const bounds2 = { x: 20, y: 20, width: 20, height: 20 }
-
-const boundsInPage0 = { x: 1, y: 1, width: 1, height: 1 }
+const boundsInPageInitial = { x: 1, y: 1, width: 1, height: 1 }
 const boundsInPage1 = { x: 100, y: 100, width: 100, height: 100 }
-const boundsInPage2 = { x: 200, y: 200, width: 200, height: 200 }
 
 const modelFactory = new SGraphFactory()
 const model = modelFactory.createRoot({ id: 'graph', type: 'graph', children: [] })
 const nodeSchema0: SNodeSchema = { id: 'node0', type: 'node:circle', x: 0, y: 0, width: 0, height: 0 }
 const nodeSchema1: SNodeSchema = { id: 'node1', type: 'node:circle', x: 0, y: 0, width: 0, height: 0 }
 
-const node0: SNode = modelFactory.createElement(nodeSchema0) as SNode
-const node1: SNode = modelFactory.createElement(nodeSchema1) as SNode
-model.add(node0)
-model.add(node1)
+const nodeBoundsAware: SNode = modelFactory.createElement(nodeSchema0) as SNode
+const nodeBoundsInPageAware: mySNode = modelFactory.createElement(nodeSchema1) as mySNode
 
-node0.bounds = bounds0
-node1.bounds = bounds0
+model.add(nodeBoundsAware)
+model.add(nodeBoundsInPageAware)
+
+nodeBoundsAware.bounds = boundsInitial
+nodeBoundsInPageAware.bounds = boundsInitial
+nodeBoundsInPageAware.boundsInPage = boundsInPageInitial
 
 const mySetboundsAction = new SetBoundsAction(
     [
-        { elementId: 'node0', newBounds: bounds1, newBoundsInPage: boundsInPage1 },
-        { elementId: 'node1', newBounds: bounds2, newBoundsInPage: boundsInPage2 }
+        { elementId: 'node0', newBounds: bounds1 },
+        { elementId: 'node1', newBounds: bounds2, newBoundsInPage: boundsInPage1 }
     ]
 )
 
@@ -52,26 +58,29 @@ const context: CommandExecutionContext = {
 describe('SetBoundsCommand', () => {
     it('execute() works as expected', () => {
         // sanity check for initial bounds values
-        expect(bounds0).deep.equals(node0.bounds)
-        expect(bounds0).deep.equals(node1.bounds)
-
+        expect(boundsInitial).deep.equals(nodeBoundsAware.bounds)
+        expect(boundsInitial).deep.equals(nodeBoundsInPageAware.bounds)
+        expect(boundsInPageInitial).deep.equals(nodeBoundsInPageAware.boundsInPage)
         cmd.execute(model, context)
 
-        expect(bounds1).deep.equals(node0.bounds)
-        expect(bounds2).deep.equals(node1.bounds)
+        expect(bounds1).deep.equals(nodeBoundsAware.bounds)
+        expect(bounds2).deep.equals(nodeBoundsInPageAware.bounds)
+        expect(boundsInPage1).deep.equals(nodeBoundsInPageAware.boundsInPage)
     })
 
     it('undo() works as expected', () => {
         cmd.undo(model, context)
-        expect(bounds0).deep.equals(node0.bounds)
-        expect(bounds0).deep.equals(node1.bounds)
+        expect(boundsInitial).deep.equals(nodeBoundsAware.bounds)
+        expect(boundsInitial).deep.equals(nodeBoundsInPageAware.bounds)
+        expect(boundsInPageInitial).deep.equals(nodeBoundsInPageAware.boundsInPage)
     })
 
     it('redo() works as expected', () => {
-        cmd.execute(model, context)
-        expect(bounds1).deep.equals(node0.bounds)
-        expect(bounds2).deep.equals(node1.bounds)
-    })
+        cmd.redo(model, context)
+        expect(bounds1).deep.equals(nodeBoundsAware.bounds)
+        expect(bounds2).deep.equals(nodeBoundsInPageAware.bounds)
+        expect(boundsInPage1).deep.equals(nodeBoundsInPageAware.boundsInPage)
+    }) 
 })
 
 
