2fd158573e817f2058b87da670285195a03ab9b2
Jan Koehnlein <jan.koehnlein@typefox.io>
2017-09-20 20:13:20 +0200

    linter again

--- client/src/graph/views.tsx
+++ client/src/graph/views.tsx
@@ -5,7 +5,7 @@
  * You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
  */
 
-import * as snabbdom from 'snabbdom-jsx';
+import * as snabbdom from 'snabbdom-jsx'
 import { VNode } from "snabbdom/vnode"
 import { center, maxDistance, Point } from "../utils/geometry"
 import { setAttr } from '../base/views/vnode-utils'
